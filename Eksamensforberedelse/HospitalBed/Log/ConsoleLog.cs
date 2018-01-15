c589502fcd0d80c156c96a1eacde0be00cca4468
Anders Røddik Petersen <anders.roddik@live.dk>
2018-01-15 23:38:54 +0100

    Tilføjet Hospital Bed 2.0

---
+++ Eksamensforberedelse/HospitalBed/Log/ConsoleLog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalBed.Log
+{
+    class ConsoleLog : ILog
+    {
+        public void LogStuff(bool presence)
+        {
+            if (presence)
+            {
+                Console.WriteLine(DateTime.Now.ToShortTimeString() + " Patient is in bed");
+            }
+            else
+            {
+                Console.WriteLine(DateTime.Now.ToShortTimeString() + " Patient has left the bed");
+            }
+        }
+    }
+}
---
+++ Eksamensforberedelse/HospitalBed/Log/FileLog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalBed.Log
+{
+    class FileLog : ILog
+    {
+        private string _path;
+
+        public FileLog()
+        {
+            _path = @"D:\Programs\Google Drive\ST3\PRG\Files\HospitalBedLogFiles";
+        }
+        public void LogStuff(bool presence)
+        {
+            using (StreamWriter writeText = new StreamWriter(_path, true))
+            {
+                if (presence)
+                {
+                    writeText.WriteLine("Patient is in bed " + DateTime.Now.ToShortTimeString());
+                }
+                else
+                {
+                    writeText.WriteLine("Patient has left the bed " + DateTime.Now.ToShortTimeString());
+                }
+            }
+        }
+    }
+}
